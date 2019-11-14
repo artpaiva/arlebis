53e40ae4e64e12f057401bd597a1be3d67d2f620
arturromero <artur.romero@vix.com>
2019-11-14 12:44:04 -0300

    Dynamic Theming Observer

--- machinery.js
+++ machinery.js
@@ -23,7 +23,9 @@ var currentNext = 1;
 var maxNext = 3;
 window.addEventListener('load', function() {
     watchNext();
-    setTheme(document.querySelector('#article-1'))
+    var firstArticle = document.querySelector('#article-1');
+    setTheme(firstArticle, true)
+    watchTheme(firstArticle);
 });
 
 function whatNext (entries, observer) {
@@ -49,30 +51,22 @@ function watchNext () {
     var next = document.createElement('article');
     var targetID = `next-${currentNext}`;
     next.id = targetID;
-    // h.previousArticle.parentNode.insertBefore(next, h.previousArticle.nextSibling);
     document.querySelector('.torso').appendChild(next);
     h.target = document.querySelector(`#${targetID}`);
     var observer = new IntersectionObserver(whatNext, options);
     observer.observe(h.target);
-    watchTheme(h.previousArticle);
 }
 
 function fillArticle (element) {
     var separator = document.createElement('div');
     separator.classList.add('next');
-    // h.previousArticle.parentNode.insertBefore(separator, h.previousArticle.nextSibling);
-    // h.previousArticle.appendChild(separator);
     element.appendChild(separator);
-    // element.classList.add('article');
     currentNext += 1;
     element.id = `article-${currentNext}`;
     element.innerHTML += document.querySelector('#article-1').innerHTML;
     randomTheme(element.id);
-    setTheme(element);
-    // var breviary = element.querySelector('.breviary');
-    // var wrapper = breviary.querySelector('.wrapper');
-    // console.log(breviary);
-    // wrapper.appendChild(separator);
+    setTheme(element, false);
+    watchTheme(element);
 }
 
 function randomTheme (id) {
@@ -83,7 +77,7 @@ function randomTheme (id) {
     breviary.setAttribute('front', fronts[Math.floor(Math.random() * fronts.length)]);
     breviary.setAttribute("back", backs[Math.floor(Math.random() * backs.length)]);
 }
-function setTheme (element) {
+function setTheme (element, all) {
     var breviary = element.querySelector('.breviary');
     var img = breviary.getAttribute('img');
     var font = breviary.getAttribute('font');
@@ -97,22 +91,27 @@ function setTheme (element) {
     breviary.style.color = title.style.borderColor = front;
     breviary.style.fontFamily = font;
     breviary.style.backgroundImage = `linear-gradient(to bottom, ${back}, ${backop}), url(${img})`;
-    cornice.style.color = front;
-    cornice.style.background = back;
+    if (all) {
+        cornice.style.color = front;
+        cornice.style.background = back;
+    }
 }
 function watchTheme (element) {
     var target = element.querySelector('.breviary');
+    console.log(target);
     let options = {
-      rootMargin: window.screen.height+'px',
-      // rootMargin: '220px',
-      threshold: 0.8,
+        rootMargin: '10px',
+        threshold: 0,
     }
-    var observer = new IntersectionObserver((entries, observer) =>{
-        entries.forEach(entry => {
-            if(entry.isIntersecting) {
-                console.log('aqui estou');
-                setTheme (element);
-            }
-        });
-    }, options);
+    var observer = new IntersectionObserver(touchBreviary, options);
+    observer.observe(target);
+}
+var wasAbove = false;
+function touchBreviary (entries, observer) {
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            console.log('ENTREI');
+            setTheme (entry.target.parentNode, true);
+        }
+    });
 }
