d695ea691101c4ecfef92032f70dadbe35c4a1ad
arturromero <artur.romero@vix.com>
2019-11-22 19:02:17 -0300

    Aesthetic Tweaks

--- machinery.js
+++ machinery.js
@@ -20,7 +20,7 @@ function turnTorsoPanel(ele) {
 
 // Loading a next article below the last article just read
 var currentNext = 1;
-var maxNext = 1;
+var maxNext = 5;
 window.addEventListener('load', function() {
     watchNext();
     var firstArticle = document.querySelector('#article-1');
